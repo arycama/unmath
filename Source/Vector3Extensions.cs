94eb772a98de2e82abf54eed4c75c2f07e325b83
Ben Sims <arycama@gmail.com>
2025-09-08 00:06:17 +0930

    Animation wips

--- Source/Vector3Extensions.cs
+++ Source/Vector3Extensions.cs
@@ -2,14 +2,14 @@
 
 public static class Vector3Extensions
 {
-    public static Vector2 XY(this Vector3 v) => new(v.x, v.y);
-    public static Vector2 XZ(this Vector3 v) => new(v.x, v.z);
+    public static Float2 XY(this Vector3 v) => new(v.x, v.y);
+    public static Float2 XZ(this Vector3 v) => new(v.x, v.z);
 
-	public static Vector3 X0(this Vector3 v) => new(0, v.y, v.z);
-    public static Vector3 Y0(this Vector3 v) => new(v.x, 0, v.z);
-    public static Vector3 Z0(this Vector3 v) => new(v.x, v.y, 0);
+	public static Float3 X0(this Vector3 v) => new(0, v.y, v.z);
+    public static Float3 Y0(this Vector3 v) => new(v.x, 0, v.z);
+    public static Float3 Z0(this Vector3 v) => new(v.x, v.y, 0);
 
-    public static Vector3 WithX(this Vector3 v, float x) => new(x, v.y, v.z);
-    public static Vector3 WithY(this Vector3 v, float y) => new(v.x, y, v.z);
-    public static Vector3 WithZ(this Vector3 v, float z) => new(v.x, v.y, z);
+    public static Float3 WithX(this Vector3 v, float x) => new(x, v.y, v.z);
+    public static Float3 WithY(this Vector3 v, float y) => new(v.x, y, v.z);
+    public static Float3 WithZ(this Vector3 v, float z) => new(v.x, v.y, z);
 }
