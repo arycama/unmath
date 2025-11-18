b0c2bd5a77f6334d1f5c4d083e2505a0a35e431d
Ben Sims <arycama@gmail.com>
2025-11-19 06:12:53 +1030

    Some tests and wip

--- Source/Math.Easing.cs
+++ Source/Math.Easing.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static partial class Math
 {
-    public static float PhysicsMoveTowards(float current, float target, ref float velocity, float acceleration, float maxDelta = float.PositiveInfinity)
+    public static float PhysicsMoveTowards(float current, float target, ref float velocity, float acceleration, float deltaTime, float maxDelta = float.PositiveInfinity)
     {
         var isNegative = target < current;
         var c = isNegative ? -Sqrt(2 * acceleration * (current - target)) : Sqrt(2 * acceleration * (target - current));
@@ -12,16 +12,16 @@ public static partial class Math
         var sqrtDiscriminant = Sqrt(Max(0, 0.5f * Square(velocity) + a1 * (target - current)));
         var halfwayTime = (sqrtDiscriminant - vs) / acceleration;
        
-        if (Time.deltaTime < halfwayTime)
+        if (deltaTime < halfwayTime)
         {
-            var result = Displacement(current, velocity, Time.deltaTime, a1);
-            velocity = Velocity(velocity, Time.deltaTime, a1);
+            var result = Displacement(current, velocity, deltaTime, a1);
+            velocity = Velocity(velocity, deltaTime, a1);
 
             // If we're too far after moving, clamp
             if (Abs(result - target) > maxDelta)
             {
                 result = target + maxDelta * Sign(result - target);
-                velocity = Sqrt(2 * acceleration * Abs(result - target)) * Sign(target - result);
+				velocity = Sqrt(2 * acceleration * Abs(result - target)) * Sign(target - result);
                 return result;
             }
 
@@ -31,7 +31,7 @@ public static partial class Math
         {
             // Overshoot check, if we're going to reach the target before deltaTime, simply return the force to reach the target
             var arrivalTime = (2 * sqrtDiscriminant - vs) / acceleration;
-            if (arrivalTime <= Time.deltaTime)
+            if (arrivalTime <= deltaTime)
             {
                 velocity = 0;
                 return target;
@@ -39,18 +39,24 @@ public static partial class Math
 
             var halfwayVelocity = Velocity(velocity, halfwayTime, a1);
             var halfwayPosition = Displacement(current, velocity, halfwayTime, a1);
-            var result = Displacement(halfwayPosition, halfwayVelocity, Time.deltaTime - halfwayTime, -a1);
+            var result = Displacement(halfwayPosition, halfwayVelocity, deltaTime - halfwayTime, -a1);
 
             // If we're too far after moving, clamp
             if (Abs(result - target) > maxDelta)
             {
                 result = target + maxDelta * Sign(result - target);
                 velocity = Sqrt(2 * acceleration * Abs(result - target)) * Sign(target - result);
-                return result;
+				return result;
             }
 
-            velocity = Velocity(halfwayVelocity, Time.deltaTime - halfwayTime, -a1);
-            return result;
+            velocity = Velocity(halfwayVelocity, deltaTime - halfwayTime, -a1);
+			return result;
         }
     }
+
+	public static float PhysicsMoveTowardsAngle(float current, float target, ref float velocity, float acceleration, float deltaTime, float maxDelta = float.PositiveInfinity)
+	{
+		target = current + Math.DeltaAngle(current, target);
+		return PhysicsMoveTowards(current, target, ref velocity, acceleration, deltaTime, maxDelta);
+	}
 }
