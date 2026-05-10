ea78c4eb223b7b69a4c921e0c2aca187db20de3c
Ben Sims <arycama@gmail.com>
2026-05-11 01:26:56 +0930

    Added some basic packing functions

---
+++ Source/Packing.cs
@@ -0,0 +1,10 @@
+﻿public static class Packing
+{
+	public static int Exp2Pow2(int x) => 1 << x;
+
+	public static float Quantize(float x, int bits) => x * (Exp2Pow2(bits) - 1) + 0.5f;
+
+	public static int BitPack(int data, int size, int offset) => (data & (Exp2Pow2(size) - 1)) << offset;
+
+	public static int BitPackFloat(float data, int size, int offset) => BitPack((int)Quantize(data, size), size, offset);
+}
